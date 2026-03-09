c83518c173b6e2c146bd7deca78ada8a1a2d3aaf
oligregz <gregorysak90@gmail.com>
2026-03-09 16:13:45 -0300

    feat(api-gateawy/env): implements global reading of variables

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as dotenvExpand from 'dotenv-expand';
 
 async function bootstrap() {
+  const env = dotenv.config();
+  dotenvExpand.expand(env);
+
   const app = await NestFactory.create(AppModule);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
