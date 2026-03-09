54ed2584f0d1d91ebee2b157540daa06aaa3183e
oligregz <gregorysak90@gmail.com>
2026-03-09 11:08:09 -0300

    feat(api-gateway/modules): config sync modules

--- api-gateway/src/auth/auth.module.ts
+++ api-gateway/src/auth/auth.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthService } from './auth.service';
 import { User } from './entities/user.entity';
+import { RateLimit, RateLimitSchema } from './schemas/rate-limit.schema';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    MongooseModule.forFeature([{ name: RateLimit.name, schema: RateLimitSchema }]),
+  ],
   providers: [AuthService],
-  exports: [TypeOrmModule],
+  exports: [TypeOrmModule, MongooseModule],
 })
 export class AuthModule {}
--- api-gateway/src/process/process.module.ts
+++ api-gateway/src/process/process.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProcessService } from './process.service';
 import { ProcessController } from './process.controller';
 import { ProcessHistory } from './entities/process-history.entity';
 import { QueueState } from './entities/queue-state.entity';
+import { ErrorLog, ErrorLogSchema } from './schemas/error-log.schema';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProcessHistory, QueueState])],
+  imports: [
+    TypeOrmModule.forFeature([ProcessHistory, QueueState]),
+    MongooseModule.forFeature([{ name: ErrorLog.name, schema: ErrorLogSchema }]),
+  ],
   providers: [ProcessService],
   controllers: [ProcessController],
 })
