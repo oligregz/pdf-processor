285c98299507938d2dd87d1cedcd12bddb379daa
oligregz <gregorysak90@gmail.com>
2026-03-10 15:11:41 -0300

    refactor(api-gateway/process): implements pipe validation

--- api-gateway/src/process/process.controller.ts
+++ api-gateway/src/process/process.controller.ts
@@ -1,17 +1,8 @@
-import { 
-  Controller, 
-  Post, 
-  UseGuards, 
-  UseInterceptors, 
-  UploadedFile, 
-  Req, 
-  ParseFilePipe, 
-  MaxFileSizeValidator, 
-  FileTypeValidator 
-} from '@nestjs/common';
+import { Controller, Post, UseGuards, UseInterceptors, UploadedFile, Req } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { ProcessService } from './process.service';
+import { PdfValidationPipe } from './pipes/pdf-validation.pipe';
 
 @Controller('process')
 export class ProcessController {
@@ -21,20 +12,13 @@ export class ProcessController {
   @UseGuards(JwtAuthGuard)
   @UseInterceptors(FileInterceptor('file'))
   async uploadPdf(
-    @UploadedFile(
-      new ParseFilePipe({
-        validators: [
-          new MaxFileSizeValidator({ maxSize: 5 * 1024 * 1024 }),
-          new FileTypeValidator({ fileType: 'application/pdf' }),
-        ],
-      }),
-    ) file: Express.Multer.File,
+    @UploadedFile(PdfValidationPipe) file: Express.Multer.File,
     @Req() req: any,
   ) {
     const userId = req.user.userId;
 
     return {
-      message: 'Upload successfuly!',
+      message: 'Upload autorizado com sucesso!',
       userId: userId,
       fileName: file.originalname,
       fileSize: file.size,
