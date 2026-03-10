cc00ef6abbf7bb34477b094b6541d2eb655fc3fa
oligregz <gregorysak90@gmail.com>
2026-03-10 15:03:11 -0300

    feat(api-gateway/controller): adds upload endpoint

--- api-gateway/src/process/process.controller.ts
+++ api-gateway/src/process/process.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { 
+  Controller, 
+  Post, 
+  UseGuards, 
+  UseInterceptors, 
+  UploadedFile, 
+  Req, 
+  ParseFilePipe, 
+  MaxFileSizeValidator, 
+  FileTypeValidator 
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { ProcessService } from './process.service';
 
 @Controller('process')
-export class ProcessController {}
+export class ProcessController {
+  constructor(private readonly processService: ProcessService) {}
+
+  @Post('upload')
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadPdf(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 5 * 1024 * 1024 }),
+          new FileTypeValidator({ fileType: 'application/pdf' }),
+        ],
+      }),
+    ) file: Express.Multer.File,
+    @Req() req: any,
+  ) {
+    const userId = req.user.userId;
+
+    return {
+      message: 'Upload successfuly!',
+      userId: userId,
+      fileName: file.originalname,
+      fileSize: file.size,
+    };
+  }
+}
