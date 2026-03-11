b406ceb8a6535f54347e28899f6f73c9e2b6464b
oligregz <gregorysak90@gmail.com>
2026-03-11 19:09:30 -0300

    chore(pdf-worker/core): initialize spring boot worker service structure

---
+++ pdf-worker/src/main/java/com/portfolio/pdfworker/PdfWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.pdfworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfWorkerApplication.class, args);
+	}
+
+}
