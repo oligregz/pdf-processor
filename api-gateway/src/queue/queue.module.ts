d4006707687aeb1d1f4dea7008ed8254753153cd
oligregz <gregorysak90@gmail.com>
2026-03-11 14:27:52 -0300

    feat(api-gateway/queue): adds messaging flow service layer

--- api-gateway/src/queue/queue.module.ts
+++ api-gateway/src/queue/queue.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { QueueService } from './queue.service';
 
 @Module({
-  providers: [QueueService]
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        name: 'RABBITMQ_SERVICE',
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.getOrThrow<string>('RABBITMQ_URL')],
+            queue: 'pdf.process.queue',
+            queueOptions: {
+              durable: true,
+            },
+          },
+        }),
+      },
+    ]),
+  ],
+  providers: [QueueService],
+  exports: [QueueService],
 })
 export class QueueModule {}
