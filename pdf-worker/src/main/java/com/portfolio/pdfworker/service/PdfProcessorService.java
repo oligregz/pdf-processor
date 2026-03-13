f7721e70d61e1ef597677655f18f48aad03f7e78
oligregz <gregorysak90@gmail.com>
2026-03-13 12:15:51 -0300

    feat(pdf-worker/pdf_processor_service): implements email data keys for shipping

--- pdf-worker/src/main/java/com/portfolio/pdfworker/service/PdfProcessorService.java
+++ pdf-worker/src/main/java/com/portfolio/pdfworker/service/PdfProcessorService.java
@@ -17,23 +17,26 @@ public class PdfProcessorService {
 	private static final Logger logger = LoggerFactory.getLogger(PdfProcessorService.class);
 	private final StorageService storageService;
 	private final DatabaseService databaseService;
+	private final EmailService emailService;
 
-	public PdfProcessorService(StorageService storageService, DatabaseService databaseService) {
+	public PdfProcessorService(StorageService storageService, DatabaseService databaseService,
+			EmailService emailService) {
 		this.storageService = storageService;
 		this.databaseService = databaseService;
+		this.emailService = emailService;
 	}
 
 	public void processPdf(PdfUploadedEvent event) throws Exception {
 		String pdfKey = event.data().storagePath();
 		String correlationId = event.data().correlationId();
+		String userEmail = event.data().email();
+		String originalFileName = event.data().originalFileName();
 
 		logger.info("Starting processing for CorrelationID: {}", correlationId);
 		databaseService.startProcessing(correlationId);
 
 		byte[] pdfBytes = storageService.downloadFile(pdfKey);
-
 		String extractedText = extractTextFromPdf(pdfBytes);
-
 		String txtKey = pdfKey.replace(".pdf", ".txt");
 
 		storageService.uploadTextFile(txtKey, extractedText);
@@ -41,6 +44,7 @@ public class PdfProcessorService {
 		databaseService.finishProcessing(correlationId);
 		logger.info("Text extraction completed and saved in: {}", txtKey);
 
+		emailService.sendCompletionEmail(userEmail, originalFileName, txtKey);
 	}
 
 	private String extractTextFromPdf(byte[] pdfBytes) throws IOException {
