004ac138e196c61936adf2f0e6db5c4c314fa66c
oligregz <gregorysak90@gmail.com>
2026-03-14 20:40:38 -0300

    feat(api-gateway/process.controller): adds swagger settings in method

--- api-gateway/src/process/process.controller.ts
+++ api-gateway/src/process/process.controller.ts
@@ -3,7 +3,11 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { ProcessService } from './process.service';
 import { PdfValidationPipe } from './pipes/pdf-validation.pipe';
+import { ApiTags, ApiOperation, ApiBearerAuth, ApiConsumes, ApiBody } from '@nestjs/swagger';
+import { FileUploadDto } from 'src/common/dtos/file-upload.dto';
 
+@ApiTags('Processamento de PDF')
+@ApiBearerAuth()
 @Controller('process')
 export class ProcessController {
   constructor(private readonly processService: ProcessService) { }
@@ -11,12 +15,18 @@ export class ProcessController {
   @Post('upload')
   @UseGuards(JwtAuthGuard)
   @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiOperation({ summary: 'Upload a PDF for background text extraction.' })
+  @ApiBody({
+    description: 'PDF file upload',
+    type: FileUploadDto,
+  })
   async uploadPdf(
     @UploadedFile(PdfValidationPipe) file: Express.Multer.File,
     @Req() req: any,
   ) {
-    const { userId, email } = req.user;
 
+    const { userId, email } = req.user;
     return await this.processService.processPdfUpload(userId, email, file);
   }
 }
