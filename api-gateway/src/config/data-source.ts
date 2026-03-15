19bdd7ec71d808620d3d111b972666728a962ecb
oligregz <gregorysak90@gmail.com>
2026-03-14 22:28:54 -0300

    feat(api-gateway/data-cource): adds migration management settings

---
+++ api-gateway/src/config/data-source.ts
@@ -0,0 +1,11 @@
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+	type: 'postgres',
+	url: process.env.NEST_POSTGRES_URL || `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
+
+	entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+	migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+
+	synchronize: false,
+});
