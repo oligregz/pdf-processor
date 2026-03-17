151250bd37b6c2257cbd0adb35d10ced7527d043
oligregz <gregorysak90@gmail.com>
2026-03-17 19:55:16 -0300

    feat(api-gateway/login): implements auth service

--- interface/src/app/features/auth/login/login.ts
+++ interface/src/app/features/auth/login/login.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
-  templateUrl: './login.html',
-  styleUrl: './login.scss'
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm: FormGroup;
   isLoading = false;
   errorMessage = '';
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
@@ -29,13 +35,17 @@ export class LoginComponent {
     this.isLoading = true;
     this.errorMessage = '';
     
-    const { email, password } = this.loginForm.value;
-    
-    console.log('Trying to log in with:', email, password);
-    
-    setTimeout(() => {
-      this.isLoading = false;
-      console.log('Simulated login successful!');
-    }, 1500);
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (res) => {
+        this.isLoading = false;
+        console.log('Login successfuly!', res);
+
+      },
+      error: (err) => {
+        this.isLoading = false;
+        console.error('Login error:', err);
+        this.errorMessage = 'Incorect credentials';
+      }
+    });
   }
 }
