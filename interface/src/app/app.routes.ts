8605f0d812d16a5a1517f39a9bb4c9f44464c755
oligregz <gregorysak90@gmail.com>
2026-03-17 08:31:22 -0300

    refact(interface/app.routes): change login path

--- interface/src/app/app.routes.ts
+++ interface/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './features/auth/login/login';
+import { LoginComponent } from './features/login/login';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
