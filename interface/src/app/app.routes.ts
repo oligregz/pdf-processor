7eaf9ebb410fe42fb4d92dd45340bc51636b7fa6
oligregz <gregorysak90@gmail.com>
2026-03-18 16:10:49 -0300

    feat(api-gateway/app.routes): adds upload route page

--- interface/src/app/app.routes.ts
+++ interface/src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login';
+import { authGuard } from './core/guards/auth-guard';
+import { Upload } from './features/upload/upload';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { 
+    path: 'upload', 
+    component: Upload,
+    canActivate: [authGuard]
+  },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
 ];
