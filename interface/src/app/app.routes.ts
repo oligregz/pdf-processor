5d1da039dd2f24a2aeb18bd3b53adccc79716f6f
oligregz <gregorysak90@gmail.com>
2026-03-18 16:21:58 -0300

    fix(api-gateway/app.routes): change import

--- interface/src/app/app.routes.ts
+++ interface/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login';
 import { authGuard } from './core/guards/auth-guard';
-import { Upload } from './features/upload/upload';
+import { UploadComponent } from './features/upload/upload';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { 
     path: 'upload', 
-    component: Upload,
+    component: UploadComponent,
     canActivate: [authGuard]
   },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
