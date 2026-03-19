859de889460ba6e9db58c6d8aa968b95d5ebe5ca
oligregz <gregorysak90@gmail.com>
2026-03-19 19:12:46 -0300

    test(interafce/download): change import

--- interface/src/app/features/download/download.component.spec.ts
+++ interface/src/app/features/download/download.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Download } from './download.component';
+import { DownloadComponent } from './download.component';
 
 describe('Download', () => {
-  let component: Download;
-  let fixture: ComponentFixture<Download>;
+  let component: DownloadComponent;
+  let fixture: ComponentFixture<DownloadComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Download]
+      imports: [DownloadComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Download);
+    fixture = TestBed.createComponent(DownloadComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
