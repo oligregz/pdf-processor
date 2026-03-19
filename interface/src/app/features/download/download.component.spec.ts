922465e63f9a35e5df155bf11f0dec87639625a0
oligregz <gregorysak90@gmail.com>
2026-03-19 19:18:40 -0300

    test(interafce/download): adds providers mock

--- interface/src/app/features/download/download.component.spec.ts
+++ interface/src/app/features/download/download.component.spec.ts
@@ -1,20 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DownloadComponent } from './download.component';
+import { ActivatedRoute } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
-describe('Download', () => {
+describe('DownloadComponent', () => {
   let component: DownloadComponent;
   let fixture: ComponentFixture<DownloadComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DownloadComponent]
+      imports: [DownloadComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (key: string) => 'joke-id-for-test'
+              }
+            }
+          }
+        }
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(DownloadComponent);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    
+    fixture.detectChanges(); 
   });
 
   it('should create', () => {
