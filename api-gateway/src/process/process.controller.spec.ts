076c916de64fdf86818ba7e776a03e9ed6b434cb
oligregz <gregorysak90@gmail.com>
2026-03-19 20:21:57 -0300

    test(api-gateway/process.controller.spec): fix test

--- api-gateway/src/process/process.controller.spec.ts
+++ api-gateway/src/process/process.controller.spec.ts
@@ -1,13 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProcessController } from './process.controller';
 import { ProcessService } from './process.service';
+import { StorageService } from '../storage/storage.service';
 
 describe('ProcessController', () => {
   let controller: ProcessController;
-  let service: ProcessService;
+  let processService: ProcessService;
+  let storageService: StorageService;
 
   const mockProcessService = {
     processPdfUpload: jest.fn(),
+  };
+
+  const mockStorageService = {
+    getFileStream: jest.fn(),
   };
 
   beforeEach(async () => {
@@ -18,15 +24,21 @@ describe('ProcessController', () => {
           provide: ProcessService,
           useValue: mockProcessService,
         },
+        {
+          provide: StorageService,
+          useValue: mockStorageService,
+        },
       ],
     }).compile();
 
     controller = module.get<ProcessController>(ProcessController);
-    service = module.get<ProcessService>(ProcessService);
+    processService = module.get<ProcessService>(ProcessService);
+    storageService = module.get<StorageService>(StorageService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
-    expect(service).toBeDefined();
+    expect(processService).toBeDefined();
+    expect(storageService).toBeDefined();
   });
 });
